0c0aafabb251b0bee1da015485ef053997d46235
sabbir-mahmud <sabbir.mahmud.zim@gmail.com>
2022-05-12 10:17:43 +0600

    warehouse models added

---
+++ apps/warehouse/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+# Create your models here.
+
+# -------------------------------------------------#
+# Warehouse product category model
+# -------------------------------------------------#
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=245)
+
+    def __str__(self):
+        return self.name
+
+# -------------------------------------------------#
+# Warehouse product model
+# -------------------------------------------------#
+
+
+class Product(models.Model):
+    choice_status = (('Active', 'Active'), ('Stored', 'Stored'),
+                     ('Damaged', 'Damaged'))
+    name = models.CharField(max_length=245)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+    price = models.FloatField()
+    serial = models.CharField(max_length=245, unique=True)
+    status = models.CharField(max_length=245, choices=choice_status)
+
+    def __str__(self):
+        return self.name
