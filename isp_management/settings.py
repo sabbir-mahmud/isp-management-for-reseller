01be5f19060e2d966f900f6fd6e887ac7def0f33
sabbir-mahmud <sabbir.mahmud.zim@gmail.com>
2022-05-14 11:15:14 +0600

    env variables added

--- isp_management/settings.py
+++ isp_management/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-38q-(ex3k6ws)3^0kk2bl^yk_t_)0_8pka6-k2f=-u0ym9p@z0'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
