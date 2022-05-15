844717f09d96fab1f3b82b2e902dfed1a0722bc8
sabbir-mahmud <sabbir.mahmud.zim@gmail.com>
2022-05-15 14:23:00 +0600

    user login view and clients update and delete view added

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -7,4 +7,7 @@ from . import views
 urlpatterns = [
     path('', views.clients_view, name='clients'),
     path('add/', views.ClientsCreateView.as_view(), name='clients_add'),
+    path('update/<str:pk>', views.ClientsUpdateView.as_view(), name='clients_update'),
+    path('delete/<str:pk>', views.ClientsDeleteView.as_view(), name='clients_delete'),
+    path('login/', views.login_view, name='login'),
 ]
--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.core.paginator import Paginator
 from django.contrib.messages.views import SuccessMessageMixin
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.auth import authenticate, login, logout
 from .models import Clients
 from .filters import ClientsFilter
 from .forms import ClientsForm
@@ -32,3 +33,47 @@ class ClientsCreateView(SuccessMessageMixin, CreateView):
     success_url = '/accounts'
     success_message = 'client was created'
     error_message = 'client was not created'
+
+# -----------------------------------#
+# clients update view
+# -----------------------------------#
+
+
+class ClientsUpdateView(SuccessMessageMixin, UpdateView):
+    model = Clients
+    template_name = 'accounts/clients_form.html'
+    form_class = ClientsForm
+    success_url = '/accounts'
+    success_message = 'client was updated'
+    error_message = 'client was not updated'
+
+
+# -----------------------------------#
+# clients delete view
+# -----------------------------------#
+
+class ClientsDeleteView(SuccessMessageMixin, DeleteView):
+    model = Clients
+    template_name = 'accounts/clients_delete.html'
+    success_url = '/accounts'
+    success_message = 'client was deleted'
+    error_message = 'client was not deleted'
+
+    # -----------------------------------#
+    # login view
+    # -----------------------------------#
+
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('clients')
+        else:
+
+            render(request, 'accounts/login.html',
+                   {'error': 'username or password is incorrect'})
+    return render(request, 'accounts/login.html')
