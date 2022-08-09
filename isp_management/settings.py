02cbc3609318290fd77d7f7ed3aabead390e14c6
sabbir-mahmud <sabbir.mahmud.zim@gmail.com>
2022-08-09 15:49:02 +0600

    updated

--- isp_management/settings.py
+++ isp_management/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ispreseller.herokuapp.com']
 
 
 # Application definition
